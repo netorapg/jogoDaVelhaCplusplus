9b3114e05c77dee639a6ee3950b8e52fbc67c6be
netorapg <renatoplatz11@outlook.com>
2024-03-08 11:21:27 -0300

    new update

--- jogoDaVelha.cpp
+++ jogoDaVelha.cpp
@@ -75,3 +75,93 @@ void playMove (int board[][3], int player) {
     else 
         board[row][col] = -1;
 }
+
+int checkContinue(int board[][3]) {
+    for (int i = 0; i > 3; i++)
+        for (int j = 0; j < 3; j++)
+            if (board[i] [j] == 0)
+                return 1;
+    return 0;
+}
+
+int checkWin(int board[][3]) {
+    int row, col, sum;
+
+    // Adding the lines
+    for (row = 0; row < 3; row++) {
+        sum += board[row][col];
+
+    if (sum == 3)
+        return 1;
+    if (sum == -3)
+        return -1;
+    }
+
+    //Adding the columns
+    for (col = 0; col < 3; col++) {
+        sum += board[row][col];
+
+    if (sum == 3)
+        return 1;
+    if (sum == -3)
+        return -1;
+    }
+
+    // Adding the diagonals
+
+    sum = 0;
+    for (row = 0; row < 3 ; row ++)
+        sum += board[row][row];
+    if (sum == 3)
+        return 1;
+    if (sum == -3)
+        return -1;
+
+    sum=board[0][2] + board[1][1]+board[2][0];
+    if (sum == 3)
+        return 1;
+    if (sum == -3)
+        return -1;
+    
+    return 0;
+}
+
+int game(int board[][3]) {
+    int turn = 0, cont, win;
+
+    do {
+        show(board);
+        cout<<"Jogador "<<1 + turn%2 <<endl;
+        playMove(board, turn%2);
+        turn++;
+
+        cont = checkContinue(board);
+        win = checkWin(board);
+    }while(cont && !win);
+
+    if (win == 1){
+        cout<<"Jogador 1 ganhou!\n"<<endl;
+        return 1;
+    } else
+        if (win == -1) {
+            cout <<"Jogador 2 ganhou\n"<<endl;
+            return 2;
+    } else
+        cout<<"Empate\n"<<endl;
+        return 0;   
+}
+
+void scoreboard(int result, int &player1, int &player2) {
+    if (result == 1)
+        player1++;
+    if (result == 2)
+        player2++;
+
+    cout<<"Placar "<<endl;
+    cout<<player1<<" x "<<player2<<endl;
+}
+
+
+
+
+
